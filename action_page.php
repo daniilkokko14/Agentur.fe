5973130dc00f6dd5db2f6cd5f32e271996215503
daniilkokko14 <73350067+daniilkokko14@users.noreply.github.com>
2020-10-27 11:47:43 +0300

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="zar.css">
+<div class=" wrapper"><form action="action_page.php">
+        <div class="container">
+          <h1>Register</h1>
+          <p>Please fill in this form to create an account.</p>
+          <hr>
+      
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" required>
+      
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required>
+      
+          <label for="psw-repeat"><b>Repeat Password</b></label>
+          <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+          <hr>
+      
+          <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+          <button type="submit" class="registerbtn">Register</button>
+        </div>
+      
+        <div class="container signin">
+          <p>Already have an account? <a href="#">Sign in</a>.</p>
+        </div>
+      </form>
+      <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(67968871, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/67968871" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
---
+++ app.js
@@ -0,0 +1,13 @@
+	/*var roubles = prompt('Введите сумму в рублях:');
+	var euro = 82;
+	var dollar = 72;
+
+	var rubToEuro = roubles/euro;
+	var rubToDollar = roubles/dollar;
+
+	alert(
+			roubles + 'рублей =' + rubToEuro + 'евро.' +
+			roubles + 'рублей =' + rubToDollar + 'доллар.'
+	);
+
+    alert ("ПРивет мир этот сайт не работает")*/
